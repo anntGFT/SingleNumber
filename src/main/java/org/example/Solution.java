2a6729d15c8f9c6dc5ef972ce17a43f24596ea8e
annt <annt@gft.com>
2022-11-25 12:01:32 +0000

    Done with for loop and with stream/bitWise

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,43 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int singleNumber(int[] nums) {
+
+        if (nums.length <= 1){
+
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        for(int i = 1; i < nums.length; i+=2) {
+
+            if(nums[i] != nums[i-1]) {
+
+                return nums[i-1];
+            }
+        }
+
+        return nums[nums.length-1];
+    }
+
+
+    //Me ha aprecido curioso como con bitWise y el operador ^ se puede hacer esto
+    public int singleNumber2(int[] nums) {
+
+        if (nums.length <= 1){
+
+            return 0;
+        }
+
+        return Arrays.stream(nums)
+                .reduce(0, (x,y) -> x ^ y);
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,53 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int result;
+    int[] nums;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void givenOneNumbers(){
+
+        result = 0;
+        nums = new int[] {5};
+
+        assertEquals(result, solution.singleNumber(nums));
+        assertEquals(result, solution.singleNumber2(nums));
+
+    }
+
+    @Test
+    public void givenThreeNumbers(){
+
+        result = 4;
+        nums = new int[] {5,4,5};
+
+        assertEquals(result, solution.singleNumber(nums));
+        assertEquals(result, solution.singleNumber2(nums));
+
+    }
+
+    @Test
+    public void givenSevenNumbers(){
+
+        result = 9;
+        nums = new int[] {5,4,5,9,4,33,33};
+
+        assertEquals(result, solution.singleNumber(nums));
+        assertEquals(result, solution.singleNumber2(nums));
+
+    }
 }
